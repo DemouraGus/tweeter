ea3b33d80af4b90e04c2a16cbfdf3f2a21157f0d
DemouraGus <demoura.gus@gmail.com>
2024-12-03 16:50:36 -0800

    Form submission using jQuery activity complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+
+    $.post($(this).attr('action'), serializedData, function(res) {
+      console.log('Sucess', res);
+    });
+  });
+});
--- public/scripts/tweet-container.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(() => {
-//   $('.tweet-container article').on('mouseover', () => {
-//     console.log('Hello')
-//     ('.tweet-container article').css('box-shadow', 'black')
-//   })
-// })
