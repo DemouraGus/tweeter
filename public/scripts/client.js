41f86fe3fbab677d5355865442bc3ef1f60fd557
DemouraGus <demoura.gus@gmail.com>
2024-12-05 09:42:01 -0800

    Responsive design activity complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     const $tweet = $(`
       <article>
       <header>
-      <span class="username"><i class="fa-regular fa-face-smile"></i>${tweetData.user.name}</span>
+      <span class="username"><img src="/images/profile-hex.png">${tweetData.user.name}</span>
       <span class="user-tag">${tweetData.user.handle}</span>
       </header>
       <div>
@@ -28,64 +28,62 @@ $(document).ready(function() {
       </footer>
       </article>`);
 
-      $tweet.find('p').append(safeText);
+    $tweet.find('p').append(safeText);
       
-      return $tweet;
-    };
+    return $tweet;
+  };
     
     
     
-    const renderTweets = function(tweets) {
-      $('.tweet-container').empty();
-      for (const tweet of tweets) {
-        const $tweet = createTweetElement(tweet)
-        $('.tweet-container').prepend($tweet);
-      }
-    };
+  const renderTweets = function(tweets) {
+    $('.tweet-container').empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.tweet-container').prepend($tweet);
+    }
+  };
     
-    const loadTweets = () => {
-      $.ajax({
-        method: 'GET',
-        url: '/tweets',
-        success: (tweets) => {
-          renderTweets(tweets);
-        }
-      });
-    };
-
-    $('form').on('submit', function(event){
-      event.preventDefault();
-      const serializedData = $(this).serialize();
-
-      // console.log($('#tweet-text').val());
-      // extract the #tweet-text value and trim empty spaces
-      const tweetText = $('#tweet-text').val().trim();
-
-      const $alertMessage = $('#alert-message');
-      $alertMessage.hide();
-
-      //If it's empty return an empty alert
-      if (!tweetText) {
-        $alertMessage.html('<i class="fa-solid fa-triangle-exclamation"></i>Tweet cannot be empty!<i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
-      } else if (tweetText.length > 140) {
-        //If it's too long return a too long alert
-        $alertMessage.html('<i class="fa-solid fa-triangle-exclamation"></i>Too long! 140 is a beatiful number and nothing can be longer!<i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
-
-      } else {
-
-        $.ajax({
-          method: 'POST',
-          url: '/tweets',
-          data: serializedData,
-          success: (res) => {
-            $('#tweet-text').val('');
-            loadTweets();
-          }
-        });
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: (tweets) => {
+        renderTweets(tweets);
       }
     });
-    
-    loadTweets();
+  };
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+
+    // extract the #tweet-text value and trim empty spaces
+    const tweetText = $('#tweet-text').val().trim();
+    const $alertMessage = $('#alert-message');
+    $alertMessage.hide();
+
+    //If it's empty return an empty alert
+    if (!tweetText) {
+      $alertMessage.html('<i class="fa-solid fa-triangle-exclamation"></i>Tweet cannot be empty!<i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
+    } else if (tweetText.length > 140) {
+      //If it's too long return a too long alert
+      $alertMessage.html('<i class="fa-solid fa-triangle-exclamation"></i>Too long! 140 is a beatiful number and nothing can be longer!<i class="fa-solid fa-triangle-exclamation"></i>').slideDown();
+
+    } else {
+
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: serializedData,
+        success: (res) => {
+          $('#tweet-text').val('');
+          loadTweets();
+        }
+      });
+    }
   });
+    
+  loadTweets();
+});
   
   
